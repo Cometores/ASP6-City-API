3e855696b8c29da3949ecd3b291bd6ebe2fd3766
cometores <anicometores@gmail.com>
2023-06-13 13:26:46 +0200

    Mail Service configuration

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -2,13 +2,19 @@
 
 public class CloudMailService: IMailService
 {
-    private string _mailTo = "admin@mycompane.com";
-    private string _mailFrom = "noreply@mycompane.com";
+    private readonly string _mailTo = string.Empty;
+    private readonly string _mailFrom = string.Empty;
+
+    public CloudMailService(IConfiguration configuration)
+    {
+        _mailTo = configuration["mailSettings:mailToAddress"];
+        _mailFrom = configuration["mailSettings:mailFromAddress"];
+    }
 
     public void Send(string subject, string message)
     {
         // send mail - output to console window
-        Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}," +
+        Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, " +
                           $"with {nameof(CloudMailService)}.");
         Console.WriteLine($"Subject: {subject}");
         Console.WriteLine($"Message: {message}");
--- CityInfo.API/Services/LocalMailService.cs
+++ CityInfo.API/Services/LocalMailService.cs
@@ -2,13 +2,19 @@
 
 public class LocalMailService : IMailService
 {
-    private string _mailTo = "admin@mycompane.com";
-    private string _mailFrom = "noreply@mycompane.com";
+    private readonly string _mailTo = string.Empty;
+    private readonly string _mailFrom = string.Empty;
+
+    public LocalMailService(IConfiguration configuration)
+    {
+        _mailTo = configuration["mailSettings:mailToAddress"];
+        _mailFrom = configuration["mailSettings:mailFromAddress"];
+    }
 
     public void Send(string subject, string message)
     {
         // send mail - output to console window
-        Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}," +
+        Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, " +
                           $"with {nameof(LocalMailService)}.");
         Console.WriteLine($"Subject: {subject}");
         Console.WriteLine($"Message: {message}");
