86ff77d3c79110bbdc741af5fb2caf93bc68298f
cometores <anicometores@gmail.com>
2023-07-11 13:24:21 +0200

    Sorting support

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,6 +1,6 @@
-﻿
-using System.Text.Json;
+﻿using System.Text.Json;
 using AutoMapper;
+using CourseLibrary.API.Entities;
 using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.ResourceParameters;
@@ -9,29 +9,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CourseLibrary.API.Controllers;
 
-[ApiController] 
+[ApiController]
 [Route("api/authors")]
 public class AuthorsController : ControllerBase
 {
     private readonly ICourseLibraryRepository _courseLibraryRepository;
     private readonly IMapper _mapper;
+    private readonly IPropertyMappingService _propertyMappingService;
 
     public AuthorsController(
         ICourseLibraryRepository courseLibraryRepository,
-        IMapper mapper)
+        IMapper mapper, IPropertyMappingService propertyMappingService)
     {
         _courseLibraryRepository = courseLibraryRepository ??
-            throw new ArgumentNullException(nameof(courseLibraryRepository));
+                                   throw new ArgumentNullException(nameof(courseLibraryRepository));
         _mapper = mapper ??
-            throw new ArgumentNullException(nameof(mapper));
+                  throw new ArgumentNullException(nameof(mapper));
+        _propertyMappingService = propertyMappingService ?? 
+                                  throw new ArgumentNullException(nameof(propertyMappingService));
     }
 
-    [HttpGet(Name = "GetAuthors")] 
+    [HttpGet(Name = "GetAuthors")]
     [HttpHead]
     public async Task<ActionResult<IEnumerable<AuthorDto>>> GetAuthors(
         [FromQuery] AuthorsResourceParameters authorsResourceParameters)
     {
         // throw new Exception("test exception");
+
+        if (!_propertyMappingService.ValidMappingExistsFor<AuthorDto, Entities.Author>(
+                authorsResourceParameters.OrderBy))
+        {
+            return BadRequest();
+        }
         
         var authorsFromRepo = await _courseLibraryRepository
             .GetAuthorsAsync(authorsResourceParameters);
@@ -39,7 +48,7 @@ public class AuthorsController : ControllerBase
         var previousPageLink = authorsFromRepo.HasPrevious
             ? CreateAuthorsResourceUri(authorsResourceParameters, ResourceUriType.PreviousPage)
             : null;
-        
+
         var nextPageLink = authorsFromRepo.HasNext
             ? CreateAuthorsResourceUri(authorsResourceParameters, ResourceUriType.NextPage)
             : null;
@@ -53,10 +62,10 @@ public class AuthorsController : ControllerBase
             previousPageLink,
             nextPageLink
         };
-        
+
         Response.Headers.Add("X-Pagination",
             JsonSerializer.Serialize(paginationMetadata));
-        
+
         return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
     }
 
@@ -70,6 +79,7 @@ public class AuthorsController : ControllerBase
                 return Url.Link("GetAuthors",
                     new
                     {
+                        orderBy = authorsResourceParameters.OrderBy,
                         pageNumber = authorsResourceParameters.PageNumber - 1,
                         pageSize = authorsResourceParameters.PageSize,
                         mainCategory = authorsResourceParameters.MainCategory,
@@ -79,6 +89,7 @@ public class AuthorsController : ControllerBase
                 return Url.Link("GetAuthors",
                     new
                     {
+                        orderBy = authorsResourceParameters.OrderBy,
                         pageNumber = authorsResourceParameters.PageNumber + 1,
                         pageSize = authorsResourceParameters.PageSize,
                         mainCategory = authorsResourceParameters.MainCategory,
@@ -88,6 +99,7 @@ public class AuthorsController : ControllerBase
                 return Url.Link("GetAuthors",
                     new
                     {
+                        orderBy = authorsResourceParameters.OrderBy,
                         pageNumber = authorsResourceParameters.PageNumber,
                         pageSize = authorsResourceParameters.PageSize,
                         mainCategory = authorsResourceParameters.MainCategory,
