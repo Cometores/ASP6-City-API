30cbb03029be3247a7e77a83328ac78df01cbab5
cometores <anicometores@gmail.com>
2023-07-13 13:30:40 +0200

    Migrations support

--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -11,6 +11,22 @@ public class PubContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
-            "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase");
+            "Server=localhost;Database=PubDatabase;User Id=sa;Password=Password123!;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>().HasData(
+            new Author { Id = 1, FirstName = "Rhoda", LastName = "Lerman" });
+
+        var authorList = new Author[]
+        {
+            new Author { Id = 2, FirstName = "Ruth", LastName = "Ozeki" },
+            new Author { Id = 3, FirstName = "Sofia", LastName = "Segovia" },
+            new Author { Id = 4, FirstName = "Ursula K.", LastName = "LeGuin" },
+            new Author { Id = 5, FirstName = "Hugh", LastName = "Howey" },
+            new Author { Id = 6, FirstName = "Isabelle", LastName = "Allende" },
+        };
+        modelBuilder.Entity<Author>().HasData(authorList);
     }
 }
