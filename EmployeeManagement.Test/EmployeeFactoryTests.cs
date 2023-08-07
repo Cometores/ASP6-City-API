d46e5624d4dca6c574be1e1c998e7698a178472d
cometores <anicometores@gmail.com>
2023-08-07 14:57:48 +0200

    Creating dummy repository for testing

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -14,4 +14,47 @@ public class EmployeeFactoryTests
         
         Assert.Equal(2500, employee.Salary);
     }
+    
+    [Fact]
+    public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500and3500()
+    {
+        var employeeFactory = new EmployeeFactory();
+
+        var employee = (InternalEmployee) employeeFactory.CreateEmployee("Andrey", "Cometores");
+        
+        Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500,
+            "Salary not in acceptable range.");
+    }
+    
+    [Fact]
+    public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500and3500_Alternative()
+    {
+        var employeeFactory = new EmployeeFactory();
+
+        var employee = (InternalEmployee) employeeFactory.CreateEmployee("Andrey", "Cometores");
+        
+        Assert.True(employee.Salary >= 2500);
+        Assert.True(employee.Salary <= 3500);
+    }
+    
+    [Fact]
+    public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500and3500_AlternativeWithInRange()
+    {
+        var employeeFactory = new EmployeeFactory();
+
+        var employee = (InternalEmployee) employeeFactory.CreateEmployee("Andrey", "Cometores");
+        
+        Assert.InRange(employee.Salary, 2500, 3500);
+    }
+    
+    [Fact]
+    public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
+    {
+        var employeeFactory = new EmployeeFactory();
+
+        var employee = (InternalEmployee) employeeFactory.CreateEmployee("Andrey", "Cometores");
+        employee.Salary = 2500.123m;
+        
+        Assert.Equal(2500, employee.Salary, 0);
+    }
 }
