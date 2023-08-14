8584815d4c9b264ed891c6214d77bec87c971443
cometores <anicometores@gmail.com>
2023-08-14 14:57:01 +0200

    Mocking an interface

--- EmployeeManagement.Test/MoqTests.cs
+++ EmployeeManagement.Test/MoqTests.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManagement.Business;
 using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.DataAccess.Services;
 using EmployeeManagement.Test.Services;
 using Moq;
 
@@ -15,10 +16,10 @@ public class MoqTests
         // var employeeFactory = new EmployeeFactory();
         var employeeFactoryMock = new Mock<EmployeeFactory>();
         var employeeService = new EmployeeService(employeeManagementTestDataRepository, employeeFactoryMock.Object);
-        
+
         // Act 
         var employee = employeeService.FetchInternalEmployee(Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
-        
+
         // Assert
         Assert.Equal(400, employee.SuggestedBonus);
     }
@@ -30,22 +31,49 @@ public class MoqTests
         var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
         var employeeFactoryMock = new Mock<EmployeeFactory>();
         employeeFactoryMock.Setup(m =>
-            m.CreateEmployee(
-                "Andrey",
-                It.IsAny<string>(),
-                null,
-                false))
+                m.CreateEmployee(
+                    "Andrey",
+                    It.IsAny<string>(),
+                    null,
+                    false))
             .Returns(new InternalEmployee("Andrey", "Cometores", 5, 2500, false, 1));
         var employeeService = new EmployeeService(employeeManagementTestDataRepository, employeeFactoryMock.Object);
-        
+
         // suggested bonus for new employees = 
         // (years in service if > 0) * attended courses * 100
         decimal suggestedBonus = 1000;
-        
+
         // Act
         var employee = employeeService.CreateInternalEmployee("Andrey", "Cometores");
-        
+
         // Assert
         Assert.Equal(suggestedBonus, employee.SuggestedBonus);
+    }
+
+    [Fact]
+    public void FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated_MoqInterface()
+    {
+        // Arrange
+        var employeeManagementTestDataRepositoryMock = new Mock<IEmployeeManagementRepository>();
+        employeeManagementTestDataRepositoryMock
+            .Setup(m => m.GetInternalEmployee(It.IsAny<Guid>()))
+            .Returns(new InternalEmployee("Tony", "Hall", 2, 2500, false, 2)
+            {
+                AttendedCourses = new List<Course>()
+                {
+                    new Course("A course"), new Course("Another course")
+                }
+            });
+
+
+        var employeeFactoryMock = new Mock<EmployeeFactory>();
+        var employeeService =
+            new EmployeeService(employeeManagementTestDataRepositoryMock.Object, employeeFactoryMock.Object);
+
+        // Act
+        var employee = employeeService.FetchInternalEmployee(Guid.Empty);
+
+        // Assert
+        Assert.Equal(400, employee.SuggestedBonus);
     }
 }
