1f16bfc71e534800c351996e3cf2aaa35c0c926c
Andrey Komissarov <anicometores@gmail.com>
2024-03-19 13:35:40 +0100

    Minimal API | Exception Middleware & Problem Details

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using DishesAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Extensions;
@@ -6,17 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// register the DbContext on the container, getting the
-// connection string from appSettings   
+// register the DbContext on the container, getting the connection string from appSettings   
 builder.Services.AddDbContext<DishesDbContext>(o => o.UseSqlite(
     builder.Configuration["ConnectionStrings:DishesDBConnectionString"]));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
 app.UseHttpsRedirection();
 
 app.RegisterDishesEndpoints();
