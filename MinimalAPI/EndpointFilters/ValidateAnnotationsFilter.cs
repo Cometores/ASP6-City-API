0d4f4927d67497e8124c7c5ccb74b1c63321c07b
Andrey Komissarov <anicometores@gmail.com>
2024-03-20 17:14:11 +0100

    Minimal API | MiniValidation & POST Validation filter

---
+++ MinimalAPI/EndpointFilters/ValidateAnnotationsFilter.cs
@@ -0,0 +1,19 @@
+﻿using MinimalAPI.Models;
+using MiniValidation;
+
+namespace MinimalAPI.EndpointFilters;
+
+public class ValidateAnnotationsFilter : IEndpointFilter
+{
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var dishForCreationDto = context.GetArgument<DishForCreationDto>(2);
+
+        if (!MiniValidator.TryValidate(dishForCreationDto, out var validationErrors))
+        {
+            return TypedResults.ValidationProblem(validationErrors);
+        }
+
+        return await next(context);
+    }
+}
--- MinimalAPI/Models/DishForCreationDto.cs
+++ MinimalAPI/Models/DishForCreationDto.cs
@@ -1,6 +1,10 @@
-﻿namespace MinimalAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalAPI.Models;
 
 public class DishForCreationDto
 {
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
     public required string Name { get; set; }
 }
